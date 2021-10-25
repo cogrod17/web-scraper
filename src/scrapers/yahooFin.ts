edb1882570998d4ee6788865cac83115d4f5c2b5
Cole Ogrodnick <patfan8@comcast.net>
2021-10-25 15:57:24 +0700

    getting table data

--- src/scrapers/yahooFin.ts
+++ src/scrapers/yahooFin.ts
@@ -5,6 +5,14 @@ import AdBlocker from "puppeteer-extra-plugin-adblocker";
 puppeteer.use(StealthPlugin());
 puppeteer.use(AdBlocker());
 
+const LEFT_TABLE_SELECTOR = "#quote-summary > div > table > tbody";
+const RIGHT_TABLE_SELECTOR =
+  "#quote-summary > div:nth-child(2) > table > tbody";
+const PERATIO_SELECTOR = "tr:nth-child(3)";
+const EPS_SELECTOR = "tr:nth-child(4)";
+const STATISTICS_TAB_SELECTOR = "#quote-nav > ul > li:nth-child(4) > a";
+const STATS_CONTAIN_SELECTOR = "#Col1-0-KeyStatistics-Proxy";
+
 const searchYahoo = async (symbol: string) => {
   const browser = await puppeteer.launch({
     // headless: false,
@@ -20,10 +28,19 @@ const searchYahoo = async (symbol: string) => {
   await page.click("#header-desktop-search-button");
   await page.waitForSelector("#quote-summary");
   console.log("landed");
-  const content = await page.content();
-  const $ = cheerio.load(content);
+  let content = await page.content();
+  let $ = cheerio.load(content);
 
-  //   getQuote($);
+  getQuote($);
+  getSummary($);
+
+  await page.click(STATISTICS_TAB_SELECTOR);
+  await page.waitForNavigation();
+
+  content = await page.content();
+  $ = cheerio.load(content);
+
+  getStatistics($);
 
   page.screenshot({ path: "yahoo.png" });
   console.log("yahoo finished");
@@ -31,12 +48,64 @@ const searchYahoo = async (symbol: string) => {
 
 searchYahoo("TSLA");
 
+function getStatistics($: Function) {
+  let data: [string, string][] = [];
+  let title: string;
+  $(STATS_CONTAIN_SELECTOR)
+    .find("tbody")
+    .each((i: number, el: HTMLElement) => {
+      $(el)
+        .find("td")
+        .each((i: number, el: HTMLElement) => {
+          if ((i + 1) % 2 !== 0) title = $(el).children("span").text();
+          else data.push([title, $(el).text()]);
+        });
+    });
+  console.log(data);
+  return data;
+}
+
+function getSummary($: Function) {
+  //   let x = $(RIGHT_TABLE_SELECTOR).find(PERATIO_SELECTOR).text();
+  let tableData: [string, string][] = [];
+
+  $(RIGHT_TABLE_SELECTOR)
+    .find("tr")
+    .each((i: number, el: HTMLElement) => {
+      let firstTd: string;
+      $(el)
+        .find("td")
+        .each((i: number, el: HTMLElement) => {
+          let cur = $(el).text();
+
+          if (i === 0) firstTd = cur;
+          else tableData.push([firstTd, cur]);
+        });
+    });
+
+  $(LEFT_TABLE_SELECTOR)
+    .find("tr")
+    .each((i: number, el: HTMLElement) => {
+      let firstTd: string;
+      $(el)
+        .find("td")
+        .each((i: number, el: HTMLElement) => {
+          let cur = $(el).text();
+
+          if (i === 0) firstTd = cur;
+          else tableData.push([firstTd, cur]);
+        });
+    });
+  console.log(tableData);
+  return tableData;
+}
+
 function getQuote($: Function) {
   let quote: string[] = [];
   let x = $("#quote-header-info div:nth-child(3) > div > div")
     .find("span")
     .each((i: number, el: HTMLElement) => {
-      quote.push($(el).text());
+      if (i <= 1) quote.push($(el).text());
     });
 
   console.log(quote);
