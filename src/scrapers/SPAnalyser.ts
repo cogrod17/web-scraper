c947467a5d8770b1cd6b37357f92722fca98f640
Cole Ogrodnick <patfan8@comcast.net>
2021-10-29 17:26:09 +0700

    asd

--- src/scrapers/SPAnalyser.ts
+++ src/scrapers/SPAnalyser.ts
@@ -1,18 +1,108 @@
 import cheerio from "cheerio";
 import axios from "axios";
+// import puppeteer from "puppeteer-extra";
+// import Stealth from "puppeteer-extra-plugin-stealth";
+// import Blocker from "puppeteer-extra-plugin-adblocker";
+// puppeteer.use(Stealth());
+// puppeteer.use(Blocker());
 
-type SymbolPrice = {
+interface Symbol {
   name: string;
   symbol: string;
   price: string | number;
   percent_change: number;
-}[];
+  PERatio?: number | string;
+  PBRatio?: number | string;
+  dividendYield?: number | string;
+  PEG?: number | string;
+  error?: string;
+}
+[];
 
-export const getSPCompanies = async () => {
+// evaluate("IBM");
+// let arr = [
+//   "JPM",
+//   "IBM",
+//   "TSLA",
+//   "AAPL",
+//   "PYPL",
+//   "RIOT",
+//   "ABBV",
+//   "T",
+//   "BAC",
+//   "TSLA",
+//   "SBUX",
+//   "SHOP",
+// ];
+
+// for (let i = 0; i < arr.length; i++) {
+//   evaluate(arr[i]);
+// }
+
+// let obj1 = {
+//   name: "cole",
+// };
+
+// let obj2 = {
+//   age: 24,
+// };
+
+// Object.assign(obj1, obj2);
+// console.log(obj1);
+
+evaluateSP();
+
+export async function evaluateSP() {
+  let list = await getSPCompanies();
+  let final: object[];
+
+  for (let i = 0; i < list.length; i++) {
+    let obj = await evaluate(list[i].symbol);
+    Object.assign(list[i], obj);
+    console.log(`${i + 1}/${list.length} symbols finished`);
+  }
+  console.log(list);
+}
+
+export async function evaluate(symbol: string) {
+  try {
+    const { data } = await axios.get(
+      `https://finance.yahoo.com/quote/${symbol}/key-statistics`
+    );
+    const $ = cheerio.load(data);
+
+    let y = $(
+      "#Col1-0-KeyStatistics-Proxy > section > div:nth-child(2) > div:nth-child(2) > div> div:nth-child(3) > div > div > table > tbody > tr:nth-child(2) > td:nth-child(2)"
+    ).text();
+    let divYield = y === "N/A" ? "N/A" : +y.slice(0, y.length - 1);
+
+    let PBRatio: string | number = $(
+      "#Col1-0-KeyStatistics-Proxy > section > div:nth-child(2) > div:nth-child(1) > div > div > div > div > table > tbody > tr:nth-child(7) > td:nth-child(2)"
+    ).text();
+    if (PBRatio !== "N/A") PBRatio = +PBRatio;
+
+    let PEG: string | number = $(
+      "#Col1-0-KeyStatistics-Proxy > section > div:nth-child(2) > div:nth-child(1) > div > div > div > div > table > tbody > tr:nth-child(5) > td:nth-child(2)"
+    ).text();
+    if (PEG !== "N/A") PEG = +PEG;
+
+    let PERatio: string | number = $(
+      "#Col1-0-KeyStatistics-Proxy > section > div:nth-child(2) > div:nth-child(1) > div > div > div > div > table > tbody > tr:nth-child(3) > td:nth-child(2)"
+    ).text();
+    if (PERatio !== "N/A") PERatio = +PERatio;
+
+    // console.log({ divYield, PBRatio, PEG, PERatio });
+    return { divYield, PBRatio, PEG, PERatio };
+  } catch (e) {
+    return { error: "there was an error" };
+  }
+}
+
+export async function getSPCompanies() {
   const { data } = await axios.get("https://www.slickcharts.com/sp500");
   const $ = cheerio.load(data);
   //   console.log($("thead").text());
-  let list: SymbolPrice = [];
+  let list: Symbol[] = [];
 
   $("tbody")
     .find("tr")
@@ -30,8 +120,5 @@ export const getSPCompanies = async () => {
       });
     });
 
-  //   console.log(list);
   return list;
-};
-
-getSPCompanies();
+}
